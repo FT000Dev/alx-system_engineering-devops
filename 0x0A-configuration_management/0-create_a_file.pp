2193fefe917c4d04041f77f67bb7ed749abb4eef
fatima zahrae  el alami chentoufi <fatimaalami1997@gmail.com>
2023-11-30 12:59:12 -0800

    dd

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,8 +0,0 @@
-#The code will  create a file name school inside the /tmp Directory
-file { '/tmp/school':
- ensure   =>   file,
- content  =>   'I live Puppet',
- mode     =>   '07744',
- owner    =>   'www-data',
- group    =>   'www-data',
-}
